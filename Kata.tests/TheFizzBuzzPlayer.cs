e72e63f620252c4ee49f722ce29f6aaa3e8cf62f
Kelsey Beard <kbeard@apterainc.com>
2017-05-22 12:05:43 -0400

    Creating Tests using TDD techniques

---
+++ Kata.tests/TheFizzBuzzPlayer.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Kata
+{
+	[TestFixture]
+	public class TheFizzBuzzPlayer
+	{
+		[TestCase(1, ExpectedResult = "1", TestName = "Given 1 returns 1.")]
+		[TestCase(2, ExpectedResult = "2", TestName = "Given 2 returns 2.")]
+		[TestCase(3, ExpectedResult = "fizz", TestName = "Given 3 returns fizz.")]
+		[TestCase(5, ExpectedResult = "buzz", TestName = "Given 5 returns buzz.")]
+		[TestCase(6, ExpectedResult = "fizz", TestName = "Given 6 returns fizz.")]
+
+		public string Calculate(int input)
+		{
+			var game = new FizzBuzzPlayer();
+			return game.Calculate(input);
+		}
+	}
+}
---
+++ Kata/FizzBuzzPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Kata
+{
+	public class FizzBuzzPlayer
+	{
+		public string Calculate(int input)
+		{
+			if (input % 3 == 0)
+				return "fizz";
+			if (input == 5)
+				return "buzz";
+			return input.ToString();
+		}
+	}
+}
